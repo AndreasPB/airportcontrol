bf6fa88b66204d18ad325dfa274921c74b63989d
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-04-22 10:00:58 +0200

    Initial commit

---
+++ src/main/java/gruppe3/airportcontrol/AirportcontrolApplication.java
@@ -0,0 +1,13 @@
+package gruppe3.airportcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportcontrolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gruppe3/airportcontrol/AirportcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppe3.airportcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportcontrolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
