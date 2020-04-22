ee5bd2bdac1e52ada63085022ce6519af1fba307
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-04-22 10:24:13 +0200

    Update AirportcontrolApplication.java

--- src/main/java/gruppe3/airportcontrol/AirportcontrolApplication.java
+++ src/main/java/gruppe3/airportcontrol/AirportcontrolApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AirportcontrolApplication {
 
+    // En masse kode!
+
     public static void main(String[] args) {
         SpringApplication.run(AirportcontrolApplication.class, args);
     }
