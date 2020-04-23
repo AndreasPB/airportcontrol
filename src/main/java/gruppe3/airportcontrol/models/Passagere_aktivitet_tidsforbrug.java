af8f095f226e4896062598a585edad320ed32e6b
Nobralyra <signees96@gmail.com>
2020-04-23 14:53:40 +0200

    Created

--- src/main/java/gruppe3/airportcontrol/models/Passagere_aktivitet_tidsforbrug.java
+++ src/main/java/gruppe3/airportcontrol/models/Passagere_aktivitet_tidsforbrug.java
@@ -1,5 +1,21 @@
 package gruppe3.airportcontrol.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 public class Passagere_aktivitet_tidsforbrug
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_passagere_aktivitet_tidsforbrug;
+
+    private int passagere_tidsforbrug;
+
+    @OneToOne
+    private Fly_stoerrelse fly_stoerrelse;
+
+    @OneToOne
+    private Passagere_aktivitet passagere_aktivitet;
 }
--- src/main/java/gruppe3/airportcontrol/repositories/PassagereAktivitetTidsforbrugRepository.java
+++ src/main/java/gruppe3/airportcontrol/repositories/PassagereAktivitetTidsforbrugRepository.java
@@ -1,5 +1,8 @@
 package gruppe3.airportcontrol.repositories;
 
-public class PassagereAktivitetTidsforbrugRepository
+import gruppe3.airportcontrol.models.Passagere_aktivitet_tidsforbrug;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassagereAktivitetTidsforbrugRepository extends JpaRepository<Passagere_aktivitet_tidsforbrug, Long>
 {
 }
