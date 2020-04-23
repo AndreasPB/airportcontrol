c48d251f90565f13cdbf9afe0c2df74b535f6f5e
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-04-23 10:18:23 +0200

    Revert "Merge branch 'master' into html-design"
    This reverts commit 40aef92224c44f49c8eb54b7f728cd4d578ffd69, reversing
    changes made to b7069175352d35992228dfdf48dbb89d0c42ed68.

--- src/main/java/gruppe3/airportcontrol/repositories/FlyRepository.java
+++
@@ -1,7 +0,0 @@
-package gruppe3.airportcontrol.repositories;
-
-import gruppe3.airportcontrol.models.Fly;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlyRepository extends JpaRepository<Fly, Long> {
-}
--- src/main/java/gruppe3/airportcontrol/repositories/KlargoeringRepository.java
+++
@@ -1,7 +0,0 @@
-package gruppe3.airportcontrol.repositories;
-
-import gruppe3.airportcontrol.models.Klargoering;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KlargoeringRepository extends JpaRepository<Klargoering, Long> {
-}
