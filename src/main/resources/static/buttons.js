8308a205e04eae3bf297389db8a5d54e50a04f74
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-04-24 09:05:16 +0200

    Ny database

--- src/main/resources/static/buttons.js
+++ src/main/resources/static/buttons.js
@@ -1,6 +1,25 @@
 $(".dropdown-item").click(function () {
 
-    let dropdownVal = $(this).attr("value");
+    // Tager data fra knappen
+    let dropdownValue = $(this).attr("value");
+    let dropdownText = $(this).text();
+    let dropdownName = $(this).attr("name");
+    let dropdownId = '#' + dropdownName;
 
-    console.log(dropdownVal);
+    // Fejlfinding
+    console.log(dropdownValue);
+    console.log(dropdownText);
+    console.log(dropdownName);
+    console.log(dropdownId);
+
+    // Skal ikke g're noget hvis Value = 0
+    if (dropdownValue === 0) {
+        $(dropdownId).html(dropdownText);
+    } else {
+        $(dropdownId).html(dropdownText);
+
+        // Skal ligge tiden til rækken under
+
+    }
+
 })
